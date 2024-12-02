8861fe36378d45b2cfa8b9b65b856236028aae82
pandashaly <shalycatori@gmail.com>
2024-12-02 02:02:49 +0000

    fix class

--- cpp00/ex01/PhoneBook.hpp
+++ cpp00/ex01/PhoneBook.hpp
@@ -6,7 +6,7 @@
 /*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 15:47:30 by ssottori          #+#    #+#             */
-/*   Updated: 2024/12/02 00:44:44 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/12/02 01:49:33 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,22 @@
 
 # include "Contact.hpp"
 
+class PhoneBook
+{
+	private:
+		Contact _contacts[8];
+		int	_size;
+		int	_old;
+	
+	public:
+		PhoneBook(void);
+		~PhoneBook(void);
+		void	createCT(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds);
+		void	addNew(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds);
+		void	overwriteOld(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds);
+		void	print_sum(int i);
+		void	print_deets(int i);
+		int		get_size(void);
+};
 
 #endif
--- cpp00/ex01/phonebook.cpp
+++ cpp00/ex01/phonebook.cpp
@@ -6,10 +6,52 @@
 /*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/01 15:43:56 by ssottori          #+#    #+#             */
-/*   Updated: 2024/12/01 15:44:20 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/12/02 02:01:25 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <cctype>
 
+PhoneBook::PhoneBook(void) : _size(0), _old(0)
+{
+}
+
+PhoneBook::~PhoneBook(void)
+{
+}
+
+void	PhoneBook::addNew(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds)
+{
+		this->_contacts[this->_size].save(fn, ln, nn, pn, ds);
+		this->size++;
+}
+
+void	PhoneBook::overwriteOld(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds)
+{
+		this->_contacts[this->_old].save(fn, ln, nn, pn, ds);
+		this->_old = (this->_old + 1) % 8;
+}
+
+void	PhoneBook::createCT(std::string fn, std::string ln, std::string nn, std::string pn, std::string ds)
+{
+	if (this->_size < 8)
+		addNew(fn, ln, nn, pn, ds);
+	else
+		overwriteOldaddNew(fn, ln, nn, pn, ds);
+}
+
+int	PhoneBook::get_size(void)
+{
+	return (this->_size);
+}
+
+void	PhoneBook::print_sum(int i)
+{
+	this->_contacts[i].print_short(i);
+}
+
+void	PhoneBook::print_deets(int i)
+{
+	this->_contacts[i].print_long(i);
+}
