3cc988717b26fec8bf5082d6d58d895d5611b333
pandashaly <shalycatori@gmail.com>
2024-12-07 02:32:41 +0000

    ex01 is a mindfuck

--- cpp02/ex00/Fixed.cpp
+++ cpp02/ex00/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/07 01:58:48 by ssottori          #+#    #+#             */
-/*   Updated: 2024/12/07 02:24:11 by ssottori         ###   ########.fr       */
+/*   Updated: 2024/12/07 02:29:55 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,13 +15,13 @@
 //This is the default constructor. sets the required values when it is called & creates object
 Fixed::Fixed(void)
 {
-    std::cout << "Constructor called." << std::endl;
+	std::cout << "Constructor called." << std::endl;
 }
 
 //Copy constructors are used to create a new element with the same values as the one passed as param
 Fixed::Fixed()
 {
-    std::cout << "Copy constructor called." << std::endl;
+	std::cout << "Copy constructor called." << std::endl;
 }
 
 //an operator overload lets us define what happens (in this case) when
@@ -29,24 +29,24 @@ Fixed::Fixed()
 //this could be done for += or -= or any other operator
 Fixed::Fixed()
 {
-    std:;cout << "Copu assignment operator called" << std::endl;
+	std::cout << "Copu assignment operator called" << std::endl;
 }
 
 /// Default constructor called when we don't the element anymore - clears mem and resources
 Fixed::~Fixed(void)
 {
-    std::cout << "Destructor called." << std::endl;
+	std::cout << "Destructor called." << std::endl;
 }
 
 int Fixed::getRawBits(void) const
 {
-    std::cout << "getRawBits member function called." << std::endl;
-    return (this->_fixedpt);
+	std::cout << "getRawBits member function called." << std::endl;
+	return (this->_fixedpt);
 
 }
 
 void    Fixed::setRawBits(int const raw)
 {
-    std::cout << "setRawBits member function called." << std::endl;
-    return ;
+	std::cout << "setRawBits member function called." << std::endl;
+	return ;
 }
---
+++ cpp02/ex01/main.cpp
@@ -0,0 +1,36 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/12/07 02:27:50 by ssottori          #+#    #+#             */
+/*   Updated: 2024/12/07 02:31:57 by ssottori         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "Fixed.hpp"
+
+int main(void)
+{
+
+	Fixed a;
+	Fixed const b(10);
+	Fixed const c(42.42f);
+	Fixed const d(b);
+
+	a = Fixed(1234.4321f);
+
+	std::cout << "a is " << a << std::endl;
+	std::cout << "b is " << b << std::endl;
+	std::cout << "c is " << c << std::endl;
+	std::cout << "d is " << d << std::endl;
+
+	std::cout << "a is " << a.toInt() << " as integer" << std::endl;
+	std::cout << "b is " << b.toInt() << " as integer" << std::endl;
+	std::cout << "c is " << c.toInt() << " as integer" << std::endl;
+	std::cout << "d is " << d.toInt() << " as integer" << std::endl;
+
+	return (0);
+}
