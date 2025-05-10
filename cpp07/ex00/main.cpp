23582d5088543fff7e4c5ba370f2442606bd023e
pandashaly <shalycatori@gmail.com>
2025-05-10 01:07:07 +0100

    starting cpp07

---
+++ cpp07/ex00/main.cpp
@@ -0,0 +1,44 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/10 01:03:39 by ssottori          #+#    #+#             */
+/*   Updated: 2025/05/10 01:06:34 by ssottori         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "whatever.hpp"
+#include <iostream>
+
+int main( void )
+{
+	std::cout << "---- PDF TEST ----" << std::endl;
+	int a = 2;
+	int b = 3;
+	
+	::swap( a, b );
+	std::cout << "a = " << a << ", b = " << b << std::endl;
+	std::cout << "min( a, b ) = " << ::min( a, b ) << std::endl;
+	std::cout << "max( a, b ) = " << ::max( a, b ) << std::endl;
+
+	std::string c = "chaine1";
+	std::string d = "chaine2";
+
+	::swap(c, d);
+	std::cout << "c = " << c << ", d = " << d << std::endl;
+	std::cout << "min( c, d ) = " << ::min( c, d ) << std::endl;
+	std::cout << "max( c, d ) = " << ::max( c, d ) << std::endl;
+	return 0;
+}
+
+// From PDF
+// Should output:
+// a = 3, b = 2
+// min(a, b) = 2
+// max(a, b) = 3
+// c = chaine2, d = chaine1
+// min(c, d) = chaine1
+// max(c, d) = chaine2
