04024e42d85df0963260b68272356b151d522198
pandashaly <shalycatori@gmail.com>
2025-05-11 14:11:54 +0100

    CPP07 DONE ✅

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -6,7 +6,7 @@
 /*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 01:02:41 by ssottori          #+#    #+#             */
-/*   Updated: 2025/05/10 13:29:32 by ssottori         ###   ########.fr       */
+/*   Updated: 2025/05/11 14:11:08 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,6 @@
 #include <iostream>
 
 // Creates an array of n elements initialized by default.
-// Tip: Try to compile int * a = new int(); then display *a.
 
 template <class T>
 class Array
@@ -24,12 +23,12 @@ class Array
 	private:
 		size_t _size;
 		T* _arr;
-	T
+	
 	public:
 	//-------- constructors & destructors --------
-		Array() : size(0) { _arr = new T[_size](); };
+		Array() : _size(0) { _arr = new T[0]; };
 
-		Array(unsigned int n) : _size(n) { _arr = new T[_size]() };
+		Array(unsigned int n) : _size(n) { _arr = new T[_size](); };
 
 		Array(const Array& other) : _size(other._size)
 		{
@@ -43,7 +42,7 @@ class Array
 	//----------------operators --------------------
 		Array& operator=(const Array& other)
 		{
-			if (this != other)
+			if (this != &other)
 			{
 				delete[] _arr;
 				_size = other._size;
@@ -54,11 +53,18 @@ class Array
 			return *this;
 		}
 
-		T& operator[](unsigned int)
+		T& operator[](unsigned int i)
 		{
 			if (i >= _size)
 				throw (std::out_of_range("Index out of bonds"));
 			return (_arr[i]);
+		}
+
+		const T& operator[](unsigned int i) const
+		{
+			if (i >= _size)
+				throw std::out_of_range("Index out of bounds");
+			return _arr[i];
 		}
 
 	//----------------member functions --------------------
--- cpp07/ex02/main.cpp
+++ cpp07/ex02/main.cpp
@@ -6,14 +6,63 @@
 /*   By: ssottori <ssottori@student.42london.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/05/10 01:02:34 by ssottori          #+#    #+#             */
-/*   Updated: 2025/05/10 11:35:38 by ssottori         ###   ########.fr       */
+/*   Updated: 2025/05/11 14:10:39 by ssottori         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Array.hpp"
 
+template <typename T>
+void printArr(const Array<T>& arr, const std::string& type)
+{
+	std::cout << type << std::endl; //type of array
+	for (unsigned int i = 0; i < arr.size(); ++i)
+		std::cout << "[" << i << "] = " << arr[i] << std::endl;
+} //we are printing the index of the array and the value
+
+
 int main()
 {
+	std::cout << std::endl;
+	std::cout << "------- int arr 5 --------" << std::endl;
+	Array<int> intArr(5);
+	for (unsigned int i = 0; i < intArr.size(); ++i)
+		intArr[i] = i * 1;
+	printArr(intArr, "intArray:");
+	//=================================
+	std::cout << std::endl;
+	std::cout << "------- OOB test --------" << std::endl;
+	try{
+		std::cout << intArr[4] << std::endl; //should work
+		std::cout << intArr[10] << std::endl;  //is oob should throw
+	}
+	catch (const std::exception &e)
+	{
+		std::cerr << "Caught exception: " << e.what() << std::endl;
+	}
+	//==================================
+	std::cout << std::endl;
+	std::cout << "------- copy constructor --------";
+	Array<int> copyArr(intArr);
+	std::cout << "\ncopyArray contents (after copy):" << std::endl;
+	for (unsigned int i = 0; i < copyArr.size(); ++i)
+		std::cout << "copyArray[" << i << "] = " << copyArr[i] << std::endl;
 
-    return 0;
+	//deep copy test
+	copyArr[0] = 42; //we are moifying coppyarr to 42
+	std::cout << "\nafter modifying:" << std::endl;
+	std::cout << "intArray[0] = " << intArr[0] << std::endl; //intArr should not be affected because its a deep copy
+	std::cout << "copyArray[0] = " << copyArr[0] << std::endl;
+
+	std::cout << std::endl;
+	std::cout << "------- str array test --------" << std::endl;
+	
+	Array<std::string> strArr(3);
+	strArr[0] = "Hello World!";
+	strArr[1] = "This is cpp07";
+	strArr[2] = "We are learning about templates and iterating through an array.";
+	printArr(strArr, "strArrat:");
+	
+
+	return 0;
 }
